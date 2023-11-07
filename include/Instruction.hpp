da7cd5157a37f4436afd67b0275f43ac8a59229d
neo <neotoxic.off@gmail.com>
2023-11-07 10:42:11 +0100

    Update Instruction.hpp

--- include/Instruction.hpp
+++ include/Instruction.hpp
@@ -5,11 +5,10 @@
     class Instruction
     {
         public:
-            Instruction(const char *, void *, uint8_t);
-            ~Instruction();
-
-        private:
             const char *disassembly;
             void *execute;
             uint8_t length;
+
+            Instruction(const char *, void *, uint8_t);
+            ~Instruction();
     };
